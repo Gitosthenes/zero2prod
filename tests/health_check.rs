58c59888f84a782106ff6c3b942d348cc86aa00b
Gitosthenes <a.bledsoe@outlook.com>
2023-04-28 10:09:32 -0600

    Ch.3.6-3.7: Add subscriber endpoint and tests

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,12 +1,62 @@
 use std::net::TcpListener;
 
 #[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    //Setup
+    let addr = spawn_app();
+    let client = reqwest::Client::new();
+    let post_body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    //Action
+    let response = client
+        .post(format!("{}/subscriptions", addr))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(post_body)
+        .send()
+        .await
+        .expect("Failed to execute POST");
+
+    //Assert
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    //Setup
+    let addr = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    //Action
+    for (post_body, error_msg) in test_cases {
+        let response = client
+            .post(format!("{}/subscriptions", addr))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(post_body)
+            .send()
+            .await
+            .expect("Failed to execute POST.");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_msg
+        )
+    }
+}
+
+#[tokio::test]
 async fn health_check_works() {
-    //Setup application and client
+    //Setup
     let addr = spawn_app();
     let client = reqwest::Client::new();
 
-    //Send GET request to health_check endpoint
+    //Action
     let response = client
         .get(&format!("{}/health_check", &addr))
         .send()
